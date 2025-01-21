9ca8a94777d91273d3e85a8e129e5e310f08a68d
cat12 <cat12@DESKTOP_NEKO>
2025-01-22 04:11:31 +0800

    加入專案檔案。

---
+++ Taiwan_Goodtea/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Taiwan_Goodtea.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
